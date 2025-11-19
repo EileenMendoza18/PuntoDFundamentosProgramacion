10cf1953a00bd57adce0f55d7a95d5be8a7e0252
EileenMendoza18 <eileenjml4816@gmail.com>
2025-11-18 21:10:01 -0500

    Punto D de fundamentos de programacion

---
+++ script.js
@@ -0,0 +1,71 @@
+// Ejercicio 1
+
+// let nombre_producto = prompt("Ingrese el nombre del producto: ");
+// let precio = parseFloat(prompt("Ingrese el valor del producto: "));
+// let cantidad = parseInt(prompt("Ingrese la cantidad comprada del producto: "));
+
+// const total_pagar= precio*cantidad;
+
+// alert(`El total a pagar por ${cantidad} ${nombre_producto} es: ${total_pagar}`);
+
+
+
+// Ejercicio 2
+// let valor_hora = parseInt(prompt("Ingrese el valor por hora trabajada: "));
+
+// let horas_trabajadas= parseInt(prompt("Ingrese la cantidad de horas trabajadas: "));
+
+// const salario_semanal= valor_hora*horas_trabajadas; 
+
+// alert(`El total a pagar del salario semanal es de: ${salario_semanal}`)
+
+
+// Ejercicio 3
+
+// let edad1 = parseInt(prompt("Ingrese la edad de la primera persona: "));
+// let edad2 = parseInt(prompt("Ingrese la edad de la segunda persona: "));
+// let edad3 = parseInt(prompt("Ingrese la edad de la tercera persona: "));
+// let edad4 = parseInt(prompt("Ingrese la edad de la cuarta personas: "));
+
+// let promedio = (edad1+ edad2+ edad3+edad4)/4;
+
+// alert(`El promedio de la edad es e: ${promedio}`);
+
+
+// Ejercicio 4
+
+// let kilometros = parseInt(prompt("Ingrese la cantidad de kilometros recorridos: "));
+// let consumo_kilometro = parseFloat(prompt("Ingrese la cantidad de litros consumidos por kilometro: "));
+
+// let litros_consumidos = kilometros*consumo_kilometro;
+
+// alert(`Los litros totales consumidos son: ${litros_consumidos}`);
+
+
+// Ejercicio 5
+
+// let distancia= parseFloat(prompt("Ingrese la distancia recorrida (Km): "));
+// let velocidad_promedio = parseFloat(prompt("Ingrese la velocidad promedio: "));
+
+// let tiempo_horas= distancia/velocidad_promedio;
+
+// alert(`El tiempo estimado en horas es de: ${tiempo_horas}`);
+
+
+
+// Ejercicio 6
+
+// let minutos = parseInt(prompt("Ingrese la cantidad de minutos:"));
+// let horas = parseInt(minutos / 60);
+// let minutosRestantes = minutos % 60;
+
+// alert(horas + " horas y " + minutosRestantes + " minutos");
+
+
+// Ejercicio 7
+
+let precio = parseFloat(prompt("Ingresa el precio del producto: "));
+let descuento = precio * 0.10;
+let precioFinal = precio - descuento;
+
+alert("El precio con el 10% de descuento es: " + precioFinal);
